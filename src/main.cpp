78901de0d7ce4e2864b3418e59d5503e1ab03105
Dmitry Ledentsov <dmlled@yahoo.com>
2016-05-26 12:17:01 +0200

    refactored the app into a class

--- src/main.cpp
+++ src/main.cpp
@@ -11,61 +11,74 @@
 #include "g3logger.h"
 #include "zeromq_log_sink.h"
 
-static void run(std::string name) {
+class cg3lz {
   crow::SimpleApp app;
-
   config cfg;
 
-  DefaultLogger default_log(cfg);
-  g3logLogger log(name, cfg.log_path);
+  DefaultLogger default_log;
+  g3logLogger log;
+  zeromq_log_sink sink;
 
-  zeromq_log_sink sink(cfg, [&default_log](std::string const& m) {
-                              default_log.log(m, crow::LogLevel::INFO);
-                            },
-                       log);
+public:
+  cg3lz(std::string const& name) : default_log(cfg), log(name, cfg.log_path),
+  sink(cfg, [this](std::string const& m) {
+    default_log.log(m, crow::LogLevel::INFO);
+  },
+  log) {
+    configure_routing();
+    configure_crow_logging();
+  }
 
-  default_log.log(std::string("Saving logs to: ") + cfg.log_path + "\n",
-                  crow::LogLevel::INFO);
+  void run() {
+    default_log.log(std::string("Saving logs to: ") + cfg.log_path + "\n",
+      crow::LogLevel::INFO);
 
-  sink.start_once();
+    sink.start_once();
 
-  CROW_ROUTE(app, "/").name("hello")([] { return "Hello World!"; });
+    app.port(cfg.port).multithreaded().run();
+  }
 
-  // echo "bla\c" | http put http://localhost:18080/log
-  CROW_ROUTE(app, "/log")
-      .methods("PUT"_method)([&log](const crow::request& req) {
-         log.log(req.body);
-         return crow::response(200);
-       });
+private:
+  void configure_crow_logging() {
+    crow::logger::setLogLevel(crow::LogLevel::DEBUG);
+    crow::logger::setHandler(&default_log);
+  }
 
-  // http get http://localhost:18080/toggle_logging
-  CROW_ROUTE(app, "/toggle_logging")
-  ([&cfg] {
-    cfg.logging = !cfg.logging;
-    return std::string("Logging now: ") + (cfg.logging ? "on" : "off");
-  });
+  void configure_routing() {
+    CROW_ROUTE(app, "/").name("hello")([] { return "Hello World!"; });
 
-  // http get http://localhost:18080/kill
-  CROW_ROUTE(app, "/kill")
-  ([&sink] {
-    sink.stop();
-    std::thread([] {
-                  std::this_thread::sleep_for(std::chrono::seconds(1));
-                  std::exit(0);
-                }).detach();
-    return "OK! Shutting down!";
-  });
+// echo "bla\c" | http put http://localhost:18080/log
+    CROW_ROUTE(app, "/log")
+    .methods("PUT"_method)([this](const crow::request& req) {
+      log.log(req.body);
+      return crow::response(200);
+    });
 
-  crow::logger::setLogLevel(crow::LogLevel::DEBUG);
-  auto handler(std::make_shared<DefaultLogger>(cfg));
-  crow::logger::setHandler(handler.get());
+// http get http://localhost:18080/toggle_logging
+    CROW_ROUTE(app, "/toggle_logging")
+    ([this] {
+      cfg.logging = !cfg.logging;
+      return std::string("Logging now: ") + (cfg.logging ? "on" : "off");
+    });
 
-  app.port(cfg.port).multithreaded().run();
-}
+// http get http://localhost:18080/kill
+    CROW_ROUTE(app, "/kill")
+    ([this] {
+      sink.stop();
+      std::thread([] {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::exit(0);
+      }).detach();
+      return "OK! Shutting down!";
+    });
+
+  }
+};
 
 int main(int argc, char* argv[]) {
   try {
-    run(argv[0]);
+    cg3lz app(argv[0]);
+    app.run();
   }
   catch (std::bad_alloc& e) {
     std::cerr << "Sorry, bad alloc: " << e.what() << std::endl;
