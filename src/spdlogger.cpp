1ab7d1bb0d50e7dde44e1b3570a97b45fc361006
Dmitry Ledentsov <dmlled@yahoo.com>
2016-05-28 14:25:56 -0700

    async logging with flushing every 2 sec

--- src/spdlogger.cpp
+++ src/spdlogger.cpp
@@ -10,9 +10,14 @@ struct spdlogger::impl {
   std::shared_ptr<spd::logger> log;
 
   // 500mb max / 10 files max
-  impl(std::string name, std::string path)
-      : log(spd::rotating_logger_mt("rotating_log", path + "/log",
-                                    1048576 * 500, 10)) {}
+  impl(std::string name, std::string path) {
+        const size_t q_size = 8192;
+        spd::set_async_mode(q_size, spdlog::async_overflow_policy::block_retry,
+                       nullptr,
+                       std::chrono::seconds(2));
+        log = spd::rotating_logger_mt("rotating_log", path + "/log",
+                                    1048576 * 500, 10);
+      }
 
   ~impl() {
     spd::drop("rotating_log");
