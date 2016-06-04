f5a0918cb38dcd9c49c1b46bf8fa7c5ca233710c
Dmitry Ledentsov <dmlled@yahoo.com>
2016-06-04 21:25:13 +0200

    rewrote main

--- src/cg3lz/app.cpp
+++ src/cg3lz/app.cpp
@@ -4,6 +4,8 @@
 #include "spdlogger.h"
 #include "zeromq_log_source.h"
 
+#include "file_contents.h"
+
 #include <stdexcept>
 
 simple_log_server::simple_log_server():
@@ -12,7 +14,10 @@ default_log(cfg.logging)
     load_config();
     configure_sink();
     configure_source();
-    configure_app();
+    configure_app_logging();
+    configure_app_routing();
+    configure_source_logging();
+    configure_views();
 }
 
 simple_log_server::~simple_log_server() {
@@ -21,8 +26,15 @@ simple_log_server::~simple_log_server() {
     } catch (...) {}
 }
 
+void simple_log_server::log(std::string const& m) {
+  sink->log(m);
+  tick();
+}
+
 void simple_log_server::run() {
-    app.run();
+  check_configuration();
+  source->start_once();
+  app.port(cfg.port).multithreaded().run();
 }
 
 void simple_log_server::shutdown() {
@@ -31,6 +43,15 @@ void simple_log_server::shutdown() {
         source->stop();
     if (sink)
         sink->shutdown();
+}
+
+void simple_log_server::tick()
+{
+  count++;
+  if (count % 100000 == 0)
+    default_log.log(
+      std::string("Received ") + std::to_string(count) + " entries",
+      crow::LogLevel::INFO);
 }
 
 void simple_log_server::load_config() {
@@ -60,19 +81,103 @@ void simple_log_server::configure_source() {
     );
 }
 
-void simple_log_server::configure_app() {
-    configure_app_logging();
-    CROW_ROUTE(app, "/")([] {
-        return "bla";
-    });
+void simple_log_server::configure_app_routing() {
+  add_front_page();
+  add_logging_rest_endpoint();
+  add_crow_logging_toggle();
+  add_kill_switch();
+  add_naive_log_file_download();
+}
+
+void simple_log_server::configure_source_logging() {
+  default_log.log(std::string("Saving logs to: ") + cfg.log_path, crow::LogLevel::INFO);
+
+  auto console = [this](std::string const& m) {
+    default_log.log(m, crow::LogLevel::INFO);
+  };
+
+  auto files = [this](std::string const& m) { log(m); };
+
+  source->configure_logging(console, files);
+}
+
+void simple_log_server::configure_views()
+{
+  index = std::unique_ptr<main_page>(new main_page(cfg.log_path, cfg.logging));
+  logs = std::unique_ptr<log_view>(new log_view(cfg.log_path));
 }
 
 void simple_log_server::check_configuration() {
-    if (!source || !sink)
+    if (!source || !sink || !index || !logs)
         throw std::runtime_error("App not configured correctly!");
 }
 
 void simple_log_server::configure_app_logging() {
     crow::logger::setLogLevel(crow::LogLevel::DEBUG);
     crow::logger::setHandler(&default_log);
+}
+
+void simple_log_server::add_kill_switch() {
+  // http get http://localhost:18080/kill
+  CROW_ROUTE(app, "/kill")
+    ([this] {
+    this->shutdown();
+    std::thread([] {
+      std::this_thread::sleep_for(std::chrono::seconds(3));
+      std::exit(0);
+    }).detach();
+    return "OK! Shutting down!";
+  });
+}
+
+void simple_log_server::add_crow_logging_toggle() {
+  // http get http://localhost:18080/toggle_logging
+  CROW_ROUTE(app, "/toggle_logging")
+    ([this] {
+    cfg.logging = !cfg.logging;
+    return std::string("Logging now: ") + (cfg.logging ? "on" : "off");
+  });
+}
+
+void simple_log_server::add_logging_rest_endpoint() {
+  // echo "bla\c" | http put http://localhost:18080/log
+  CROW_ROUTE(app, "/log")
+    .methods("PUT"_method)([this](const crow::request& req) {
+    log(req.body);
+    return crow::response(200);
+  });
+}
+
+void simple_log_server::add_front_page() {
+  CROW_ROUTE(app, "/").name("front_end")([this] {
+    auto response = crow::response(index->render());
+    response.set_header("Content-Type", "text/html;charset=UTF-8");
+    return response;
+  });
+}
+
+void simple_log_server::add_naive_log_file_download() {
+  CROW_ROUTE(app,
+    "/logs/<string>")([this](std::string filename)->crow::response {
+    auto file = file_contents(logs->path_for_filename(filename));
+    if (!file.exists()) {
+      auto response = crow::response();
+      response.code = 404;
+      return response;
+    }
+    else {
+      try {
+        auto response = crow::response(file.contents());
+        response.set_header(
+          "Content-Type",
+          "application/octet-stream");  // or set to whatever you need
+        return response;
+      }
+      catch (std::bad_alloc&) {
+        auto response = crow::response(std::string("Sorry, not enough memory to download ") + filename);
+        response.code = 500;
+        return response;
+      }
+    }
+  });
 }
--- src/cg3lz/app.h
+++ src/cg3lz/app.h
@@ -6,7 +6,13 @@
 #include "default_logger.h"
 #include "config_persistence.h"
 
+#include "main_page.h"
+#include "log_view.h"
+
 #include <memory>
+#include <string>
+#include <atomic>
+#include <cstdint>
 
 class spdlogger;
 class zeromq_log_source;
@@ -19,19 +25,33 @@ class simple_log_server {
     std::unique_ptr<zeromq_log_source> source;
     crow::SimpleApp app;
 
+    std::unique_ptr<main_page> index;
+    std::unique_ptr<log_view> logs;
+
+    std::atomic<std::uint64_t> count = 0;
+
 public:
     simple_log_server();
     ~simple_log_server();
 public:
+    void log(std::string const& m);
     void run();
     void shutdown();
 private:
+    void tick();
     void load_config();
     void save_config();
     void configure_sink();
     void configure_source();
-    void configure_app();
+    void configure_app_routing();
+    void configure_app_logging();
+    void configure_source_logging();
+    void configure_views();
     void check_configuration();
 private:
-    void configure_app_logging();
+    void add_front_page();
+    void add_logging_rest_endpoint();
+    void add_crow_logging_toggle();
+    void add_kill_switch();
+    void add_naive_log_file_download();
 };
